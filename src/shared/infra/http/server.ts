94b65bf91eb58bff73860f53553ee86e26f82c5d
Gabriel Araujo <gabriel@kenzie.com.br>
2020-12-08 08:58:49 -0300

    fix: cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import cors from 'cors'
 
 import '@shared/infra/typeorm';
 
@@ -11,6 +12,7 @@ import '@shared/container';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
